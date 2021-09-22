592ead718e471542f663471fa8128b376c592e11
Valera <Valera@DESKTOP-L8F6GF3>
2021-09-22 16:44:50 +0700

    Добавьте файлы проекта.

---
+++ ExtremumConsolе/FuncClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace First_Task
+{
+    public class Func
+    {
+        const double golden_ret = 1.618; //золотое сечение
+        String f = "y=(x+1)^3+5x^2";
+
+
+        double CalcFunc(double x)
+        {
+            return x = Math.Pow(x + 1, 3) + 5 * (x * x);
+        }
+        public double GetExtremumMin(double a, double b, double e) //a и b интервал, е точность
+        {
+
+            double x = 0;
+            while (Math.Abs(b - a) > e)
+            {
+
+                double x1 = b - ((b - a) / golden_ret);
+                double x2 = a + ((b - a) / golden_ret);
+
+                double y1 = CalcFunc(x1);
+                double y2 = CalcFunc(x2);
+
+                if (y1 >= y2) a = x1;
+                else b = x2;
+
+                if (Math.Abs(b - a) < e)
+                {
+                    x = (a + b) * 0.5;
+                    break;
+                }
+            }
+            return x;
+        }
+
+        public double GetExtremumMax(double a, double b, double e)
+        {
+
+            double x = 0;
+            while (Math.Abs(b - a) > e)
+            {
+
+                double x1 = b - ((b - a) / golden_ret);
+                double x2 = a + ((b - a) / golden_ret);
+
+                double y1 = CalcFunc(x1);
+                double y2 = CalcFunc(x2);
+
+                if (y1 <= y2) a = x1;
+                else b = x2;
+
+                if (Math.Abs(b - a) < e)
+                {
+                    x = (a + b) * 0.5;
+                    break;
+                }
+            }
+            return x;
+        }
+
+        public String ToString()
+        {
+            return f;
+        }
+    }
+}
---
+++ ExtremumConsolе/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace First_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func new_func = new Func();
+
+            Console.WriteLine(new_func.ToString());
+            Console.WriteLine("\nПри интервале [-2;2]");
+            Console.WriteLine("Точка минимума функции:x=" + new_func.GetExtremumMin(-2, 2, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + new_func.GetExtremumMax(-2, 2, 0.001));
+
+            Console.WriteLine("\nПри интервале [-8;8]");
+            Console.WriteLine("Точка минимума функции:x=" + new_func.GetExtremumMin(0, 8, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + new_func.GetExtremumMax(0, 8, 0.001));
+
+        }
+    }
+}
