1f0ecd47c3a14cefbefc26e6cc6e2d989e13edd2
Василий Быстрицкий <sc-2005@yandex.ru>
2021-09-25 01:09:51 +0700

    Отредачил всё по рекоммендациям

---
+++ ExtremumConsolе/GoldenSection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace First_Task
+{
+    public class GoldenSection
+    {
+        private const double FI = 1.618;
+
+        /// <summary>
+        /// Метод поиска минимума функции на заданном отрезке
+        /// </summary>
+        /// <param name="func">Задання функция.</param>
+        /// <param name="a">Начало отрезка.</param>
+        /// <param name="b">Конец отрезка.</param>
+        /// <param name="e">Точность.</param>
+        /// <returns></returns>
+        public double GetExtremumMin(Func<double, double> func, double a, double b, double e)
+        {
+
+            double x1 = 0;
+            double x2 = 0;
+
+            while (true)
+            {
+                x1 = b - ((b - a) / FI);
+                x2 = a + ((b - a) / FI);
+
+                if (func(x1) >= func(x2))
+                    a = x1;
+                else
+                    b = x2;
+
+                if (Math.Abs(b - a) < e)
+                {
+                    x1 = (a + b) * 0.5;
+                    break;
+                }
+            }
+            return x1;
+        }
+
+        /// <summary>
+        /// Метод поиска максимума фунции на заданном отрезке
+        /// </summary>
+        /// <param name="a">Начало отрезка.</param>
+        /// <param name="b">Конец отрезка.</param>
+        /// <param name="e">Точность.</param>
+        /// <returns></returns>
+        public double GetExtremumMax(Func<double, double> func, double a, double b, double e)
+        {
+            double x1 = 0;
+            double x2 = 0;
+
+            while (true)
+            {
+                x1 = b - ((b - a) / FI);
+                x2 = a + ((b - a) / FI);
+
+                if (func(x1) <= func(x2))
+                    a = x1;
+                else
+                    b = x2;
+
+                if (Math.Abs(b - a) < e)
+                {
+                    x1 = (a + b) * 0.5;
+                    break;
+                }
+            }
+            return x1;
+        }
+
+
+    }
+}
--- ExtremumConsolе/Program.cs
+++ ExtremumConsolе/Program.cs
@@ -4,18 +4,22 @@ namespace First_Task
 {
     class Program
     {
+        static double CalcFunc(double x)
+        {
+            return Math.Pow(x + 1, 3) + 5 * (x * x);
+        }
+
         static void Main(string[] args)
         {
-            Func new_func = new Func();
-
-           
+            GoldenSection new_func = new GoldenSection();
             Console.WriteLine("\nПри интервале [-2;2]");
-            Console.WriteLine("Точка минимума функции:x=" + new_func.GetExtremumMin(-2, 2, 0.001));
-            Console.WriteLine("Точка максимума функции:x=" + new_func.GetExtremumMax(-2, 2, 0.001));
-
+            Console.WriteLine("Точка минимума функции:x=" + new_func.GetExtremumMin(CalcFunc, -2, 2, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + new_func.GetExtremumMax(CalcFunc, -2, 2, 0.001));
             Console.WriteLine("\nПри интервале [-8;8]");
-            Console.WriteLine("Точка минимума функции:x=" + new_func.GetExtremumMin(0, 8, 0.001));
-            Console.WriteLine("Точка максимума функции:x=" + new_func.GetExtremumMax(0, 8, 0.001));
+            Console.WriteLine("Точка минимума функции:x=" + new_func.GetExtremumMin(CalcFunc, 0, 8, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + new_func.GetExtremumMax(CalcFunc, 0, 8, 0.001));
+
+            
 
         }
     }
